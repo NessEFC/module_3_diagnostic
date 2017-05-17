aac7367d2b43b91c1888865039b524b40f3e177e
NessEFC <NessEFC@gmail.com>
2017-05-17 10:42:05 -0600

    can parse out station data from reponse hash in controller, must refactor

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,12 +1,15 @@
 class SearchController < ApplicationController
 
   def index
-    
     response = Faraday.get("https://api.data.gov/nrel/alt-fuel-stations/v1/nearest.json?limit=10&api_key=#{ENV['NREL_KEY']}&location=80203&radius=6.0&fuel_type=ELEC,LPG")
-binding.pry
-    raw_stations = JSON.parse(response.body, symbolize_names: true)
 
+    raw_data = JSON.parse(response.body, symbolize_names: true)
+
+    raw_stations = raw_data[:fuel_stations]
+
+    @station_data = raw_stations.map do |station|
+      station[:station_name]
+    end
   end
-
 
 end
