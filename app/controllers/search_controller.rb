727d438f60946cc9f311c89fc59aa422fdf9b176
NessEFC <NessEFC@gmail.com>
2017-05-17 10:30:59 -0600

    make the API call for nearest stations and parse reponse body

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,12 @@
 class SearchController < ApplicationController
 
   def index
+    
+    response = Faraday.get("https://api.data.gov/nrel/alt-fuel-stations/v1/nearest.json?limit=10&api_key=#{ENV['NREL_KEY']}&location=80203&radius=6.0&fuel_type=ELEC,LPG")
+binding.pry
+    raw_stations = JSON.parse(response.body, symbolize_names: true)
 
   end
+
 
 end
--- spec/features/visitor_sees_list_of_nearest_stations_spec.rb
+++ spec/features/visitor_sees_list_of_nearest_stations_spec.rb
@@ -8,7 +8,7 @@ RSpec.feature "As a vistior" do
 
         visit '/'
 
-        fill_in 'q', :with => '80203'
+        fill_in 'search', :with => '80203'
         click_on "Locate"
 
         expect(current_path).to eq(search_path)
